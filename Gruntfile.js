cb4064016773d4bc7658ca4741073340b82a8409
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2015-05-29 01:03:09 +0100

    added jscs grunt task, fixed few reported errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
- 
+
   // Project configuration.
   grunt.initConfig({
     // This line makes your node configurations available for use
@@ -20,11 +20,32 @@ module.exports = function(grunt) {
       test: {
         src: ['test/eg-helper-test.js']
       }
+    },
+
+    jscs: {
+      options: {
+        config: '.jscsrc'
+      },
+
+      // just lint the source dir
+      source: {
+        src: ['lib/**/*.js']
+      },
+
+      // fix any linting errors that can be fixed
+      fixup: {
+        src: ['lib/**/*.js'],
+        options: {
+          fix: true
+        }
+      }
     }
+
   });
   // Each plugin must be loaded following this pattern
   grunt.loadNpmTasks('grunt-contrib-jshint');
- 
+  grunt.loadNpmTasks('grunt-jscs');
+
 
   // Custom task(s).
   // First argument names the task for use in command line
